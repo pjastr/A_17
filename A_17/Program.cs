b2093c43db38b05dd9af90ec60a22705416e6756
Piotrek <@>
2017-10-26 15:46:14 +0200

    przesłanie plików

---
+++ A_17/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę w systemie szesnastkowym");
+            string liczba16 = Console.ReadLine();
+            int liczba10 = int.Parse(liczba16, System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine("Liczba w systemie dziesiętnym: {0}", liczba10);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Napisz program zamieniający liczbę w systemie szesnastkowym na system dziesiętny.
+
+Przykładowe wejście: 
+
+ABC456
+
+Przykładowe wyjście: 
+
+11256918
